b8cd1a927f1f19561ab5ff225063676b2c1d98a4
Karthik <jeefkarthik036@gmail.com>
2022-05-19 11:31:59 +0530

    Get request by using Path variable

--- main/java/com/example/demoApp/controller/RestController.java
+++ main/java/com/example/demoApp/controller/RestController.java
@@ -1,21 +1,30 @@
 package com.example.demoApp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demoApp.model.User;
+import org.springframework.web.bind.annotation.*;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/hello")
 public class RestController {
 
+    //localhost:8080/hello -w "\n"
     @RequestMapping(value = {"", "/", "home"})
     public String sayHello() {
         return "Hello World!!UC-1.";
     }
 
+    //localhost:8080/hello/query?name=Mark
     @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(value= "name")String name){
-        return "Hello "+ name +" From Bridgelabz";
+        return "Hello "+ name +" From Bridgelabz!!UC-2";
     }
+
+    //curl localhost:8080/hello/param/Mark -w
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello " + name + " From Bridgelabz!!UC-3";
+    }
+
+
 }
