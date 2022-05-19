097c512b7c3084ab3ed950ebf6893cfeee686102
Karthik <jeefkarthik036@gmail.com>
2022-05-19 11:00:55 +0530

    Added REST Call to show name using GET request method

--- main/java/com/example/demoApp/controller/RestController.java
+++ main/java/com/example/demoApp/controller/RestController.java
@@ -2,6 +2,8 @@ package com.example.demoApp.controller;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("/hello")
@@ -10,5 +12,10 @@ public class RestController {
     @RequestMapping(value = {"", "/", "home"})
     public String sayHello() {
         return "Hello World!!UC-1.";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value= "name")String name){
+        return "Hello "+ name +" From Bridgelabz";
     }
 }
